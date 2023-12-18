6bf3c50170871c8a3d8f91af09292b8e1f0c5169
redditsuns <bruxc@sunsreddit.net>
2023-12-17 22:06:13 -0500

    Adds Reddit API integration

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,3 +1,4 @@
+import reddit from '../src/reddit-api.mjs';
 import { TeamRosterTable } from '../src/roster.mjs';
 import { MonthlyScheduleGenerator } from '../src/schedule.mjs';
 import { default as parameters } from '../config/parameters.json' assert { type: 'json' };
@@ -6,6 +7,8 @@ import { default as parameters } from '../config/parameters.json' assert { type:
   const generateSchedule = new MonthlyScheduleGenerator(parameters);
   const schedule = await generateSchedule.generateMonthlySchedule();
   const roster = await TeamRosterTable();
-  console.log(schedule + roster);
-  return schedule + roster;
+  const subreddit = await reddit.getSubreddit(parameters.reddit.subreddit.name);
+  await subreddit.editSettings({description: schedule + roster});
+  // console.log(schedule + roster);
+  // return schedule + roster;
 })();
