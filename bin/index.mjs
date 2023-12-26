f4eca072014dc596c302aa2126883fa2b95cdb77
redditsuns <bruxc@sunsreddit.net>
2023-12-26 17:56:42 -0500

    Updates entry point with newest supported table outputs

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,11 +1,42 @@
-import { TeamRosterTable } from '../src/roster.mjs';
-import { MonthlyScheduleGenerator } from '../src/schedule.mjs';
-import { default as parameters } from '../config/parameters.json' assert { type: 'json' };
+// import reddit from '../src/reddit-api.mjs';
+import { discordTable } from "../src/discordTable.mjs";
+import { teamRecordTable } from "../src/record.mjs";
+import { divisionStandingsTable } from "../src/standing.mjs";
+import { MonthlyScheduleGenerator } from "../src/schedule.mjs";
+import { TeamRosterTable } from "../src/roster.mjs";
+import { hyperlinkTables } from "../src/hyperlink.mjs";
+import { default as parameters } from "../config/parameters.json" assert { type: "json" };
+import { formatTable } from "../src/helpers.mjs";
 
 (async () => {
+  /* User input parameters */
+  const { reddit: { subreddit }, discord, nba: { TeamID } } = parameters;
+
+  /* Banner Tables */
+  const _discordTable = discordTable(subreddit.long_name, discord.url);
+  const _teamRecordTable = await teamRecordTable(TeamID);
+
+  /* Sidebar Tables */
+  const _divisionStandingsTable = await divisionStandingsTable(TeamID);
+
+  // Will likely refactor; class seems a little overkill
   const generateSchedule = new MonthlyScheduleGenerator(parameters);
-  const schedule = await generateSchedule.generateMonthlySchedule();
-  const roster = await TeamRosterTable();
-  console.log(schedule + roster);
-  return schedule + roster;
+  const _monthlyTeamSchedule = await generateSchedule.generateMonthlySchedule();
+  const _teamRosterTable = await TeamRosterTable();
+  const _hyperlinkTables = hyperlinkTables(parameters.reddit.sections);
+  const _sidebarFooter = '\n[BACK TO TOP](#top)';
+  const results =
+    _discordTable +
+    formatTable(_teamRecordTable) +
+    formatTable(_divisionStandingsTable) +
+    formatTable(_monthlyTeamSchedule) +
+    formatTable(_teamRosterTable) +
+    _hyperlinkTables +
+    formatTable(_sidebarFooter);
+  console.log(results);
+  return results;
 })();
+
+// const subreddit = await reddit.getSubreddit(parameters.reddit.subreddit.r);
+// await subreddit.editSettings({description: schedule + roster});
+// return schedule + roster;
